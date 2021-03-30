fd3f5159a15bf933f0f48785f1f445217d394e97
ToniTrilhas <Pichau@DESKTOP-9LK3UQP>
2021-03-30 11:22:21 -0300

    Login Screen
    Login Screen Generated.

--- new-trilhas-panel/src/Router/Routers.js
+++ new-trilhas-panel/src/Router/Routers.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Clients from "../Screen/Clients";
-import HomeScreen from "../Screen/Home";
+import Home from "../Screen/Home";
+import Login from "../Screen/Login";
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path={"/home"}>
-          <HomeScreen />
+          <Home />
         </Route>
         <Route exact path={"/clients"}>
           <Clients />
+        </Route>
+        <Route exact path={"/"}>
+          <Login />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ new-trilhas-panel/src/Screen/Login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <Container>
+      </Container>
+    );
+  }
+}
