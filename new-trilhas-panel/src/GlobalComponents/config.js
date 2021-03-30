01ea512aeaec53dec6085fc35e15286d37f55f1d
Felipe Polato <66489612+felipepolato@users.noreply.github.com>
2021-03-30 13:35:30 -0300

    interação firebase
    interação firebase com componente config

---
+++ new-trilhas-panel/src/GlobalComponents/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQC9e8KmE5UVhJVCvH60Jh9F59FouJcBQ",
+  authDomain: "trilhas-f0c85.firebaseapp.com",
+  databaseURL: "https://trilhas-f0c85-default-rtdb.firebaseio.com",
+  projectId: "trilhas-f0c85",
+  storageBucket: "trilhas-f0c85.appspot.com",
+  messagingSenderId: "249334929783",
+  appId: "1:249334929783:web:79070fd234c1deff8da972",
+  measurementId: "G-SHFCQ2BZHK",
+};
+
+export const fire = firebase.initializeApp(firebaseConfig);
